44f9e2521174e079e4dbaa67b4c9b873d1ab6507
sovonthy <sovonthy.leng069@gmail.com>
2021-11-02 13:56:29 +0700

    - Initialize project

---
+++ app/src/main/java/com/group3/online_shop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.group3.online_shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
